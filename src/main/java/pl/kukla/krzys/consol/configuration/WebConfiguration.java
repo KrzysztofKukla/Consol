3ff8af1ef069f72ea1789499ba1dc87222da3fba
krzys <krzys.kukla@gmail.com>
2016-01-24 00:10:45 +0100

    resources messageSource

--- src/main/java/pl/kukla/krzys/consol/configuration/WebConfiguration.java
+++ src/main/java/pl/kukla/krzys/consol/configuration/WebConfiguration.java
@@ -3,8 +3,10 @@ package pl.kukla.krzys.consol.configuration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.ReloadableResourceBundleMessageSource;
 import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
@@ -16,13 +18,29 @@ import org.springframework.web.servlet.view.InternalResourceViewResolver;
  */
 @Configuration
 @EnableWebMvc
-@ComponentScan("pl.kukla.krzys.consol.web")
+@ComponentScan("pl.kukla.krzys.consol")
 public class WebConfiguration extends WebMvcConfigurerAdapter{
+	
+	@Override
+	public void addResourceHandlers(ResourceHandlerRegistry registry) {
+		registry.addResourceHandler("/resources/**").addResourceLocations("/resources/");
+		super.addResourceHandlers(registry);
+	}
+	
 	@Bean
 	public ViewResolver view(){
 		InternalResourceViewResolver view =  new InternalResourceViewResolver();
 		view.setPrefix("/WEB-INF/views/");
 		view.setSuffix(".jsp");
 		return view;
+	}
+	
+	@Bean
+	public static ReloadableResourceBundleMessageSource messageSource(){
+		ReloadableResourceBundleMessageSource messageSource = new ReloadableResourceBundleMessageSource();
+		String[] resources = {"classpath:messages"};
+		messageSource.setBasenames(resources);
+		messageSource.setCacheSeconds(1);
+		return messageSource;
 	}
 }
